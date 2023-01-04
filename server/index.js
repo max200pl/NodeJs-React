a5205874b203da0479ae66ce848cba43b683c343
Maksym Poskannyi <maksym.poskannyi@avanquest.com>
2023-01-04 10:24:41 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config() // для того чтобы считывать файлы .env
+const express = require("express")
+const sequelize = require('./db')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+const start = async () => {
+    try {
+        await sequelize.authenticate() // подключения к базе данных 
+        await sequelize.sync() // сверяет состояние базы данных со схемой данных 
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`)) // .listen() какой порт должен прослушивать наш сервер 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+start()
